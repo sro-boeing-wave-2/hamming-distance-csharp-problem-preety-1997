0ed7febdd6138d23bb42bb266cdc7889fa35940b
Preety Kumari <preetydakshana@gmail.com>
2018-07-19 12:14:04 +0530

    solution for hamming distance updated

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -2,13 +2,33 @@
 
 namespace Hamming
 {
-    public class Hamming
-    {
-        public static int Distance(string original, string current)
-        {
-            int hammingDistance = 0;
-            
-            return hammingDistance;
-        }
-    }
+	public class Hamming
+	{
+		public static int Distance(string original, string current)
+		{
+			int hammingDistance = 0;
+			if (original == null)
+			{
+				throw new ArgumentNullException("original");
+			}
+			else if (current == null)
+			{
+				throw new ArgumentNullException("current");
+			}
+			else if (current.Length != original.Length)
+			{
+				throw new ArgumentException("Hamming Distance can only be calculated over strings of equal length");
+
+			}
+			else
+			{
+				for (int i = 0; i < original.Length; i++)
+				{
+					if (original[i] != current[i])
+						hammingDistance++;
+				}
+			}
+			return hammingDistance;
+		}
+	}
 }
